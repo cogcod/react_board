58378664c0d520f850988d587df51408cc15900f
jenna <jenna@greenit.cc>
2022-08-18 18:11:23 +0900

    글쓰기 버튼 기능 추가

--- src/page/PostList.js
+++ src/page/PostList.js
@@ -3,33 +3,39 @@ import CommonTable from "../table/CommonTable";
 import CommonTableRow from "../table/CommonTableRow";
 import CommonTableColumn from "../table/CommonTableColumn";
 import { postData } from "../data/Data";
-import {Link} from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
-function PostList(props) {
+function PostList() {
     const [ dataList, setDataList ] = useState([]);
+    const navigate = useNavigate();
 
     useEffect(()=>{
-        setDataList(postData);
+        setDataList(postData);  // postData 유저 데이터를 setDataList를 통해 dataList 값으로 넣어라
     }, [])
+
+    function onClickNewPost(){
+        navigate("/PostNew")
+    }
 
     return (
         <>
             <CommonTable header={['글번호', '제목', '등록일', '조회수']} >
             {
-                dataList ? dataList.map((item, index) => {
+                dataList ? dataList.map((item, index) => {  // dataList가 있으면? 각 유저를 돌면서 아래 태그로 데이터 뿌리기
                     return (
                         <CommonTableRow key={index}>
                             <CommonTableColumn>{item.no}</CommonTableColumn>
                             <CommonTableColumn>
                                 <Link to={`/postView/${item.no}`}>{item.title}</Link>
                             </CommonTableColumn>
-                            <CommonTableColumn>{item.createData}</CommonTableColumn>
+                            <CommonTableColumn>{item.createDate}</CommonTableColumn>
                             <CommonTableColumn>{item.readCount}</CommonTableColumn>
                         </CommonTableRow>
                     )
                 }) : ''
             }
             </CommonTable>
+            <button onClick={onClickNewPost}>글쓰기</button>
         </>
     );
 }
