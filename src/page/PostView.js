b97e1b24017e3d79a1c75fd98a0551a5690aa543
jenna <jenna@greenit.cc>
2022-08-18 18:09:18 +0900

    '목록으로 돌아가기'버튼 기능 - useNavigate

--- src/page/PostView.js
+++ src/page/PostView.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { getPostByNo } from "../data/Data";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import './PostList.css';
 
 function PostView() {
     const params = useParams();
     const findData = getPostByNo(params.no);
+    let history = useNavigate();
 
     return (
         <>
@@ -17,26 +18,34 @@ function PostView() {
                             <div className="post-view-row">
                                 <label>게시글 번호</label>
                                 <label>{ findData.no }</label>
-                            </div><div className="post-view-row">
+                            </div>
+                            <div className="post-view-row">
                                 <label>제목</label>
                                 <label>{ findData.title }</label>
-                            </div><div className="post-view-row">
+                            </div>
+                            <div className="post-view-row">
                                 <label>작성일</label>
                                 <label>{ findData.createDate }</label>
-                            </div><div className="post-view-row">
+                            </div>
+                            <div className="post-view-row">
                                 <label>조회수</label>
                                 <label>{ findData.readCount }</label>
-                            </div><div className="post-view-row">
+                            </div>
+                            <div className="post-view-row">
                                 <label>내용</label>
                                 <div>{ findData.content }</div>
                             </div>
                         </>
                     ) : "해당 게시글을 찾을 수 없습니다."
                 }
-                <button className="post-view-go-list-btn" onClick={()=> console.log("버튼")}>목록으로 돌아가기</button>
+
+                <button className="post-view-go-list-btn" onClick={
+                    // ()=> window.location.href="/"
+                    // () => {window.history.back()}
+                    () => history("/")
+                }>목록으로 돌아가기</button>
             </div>
         </>
-
     );
 }
 
